e6c6b15af390cfca4224ddc9cf82a8ea0966f626
Adrian Vogelsgesang <avogelsgesang@salesforce.com>
2024-09-21 22:28:55 +0000

    Hide inner instead of outer context

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -321,7 +321,7 @@ require('treesitter-context').setup{
   min_window_height = 6, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
   line_numbers = true,
   multiline_threshold = 1, -- Maximum number of lines to show for a single context
-  trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+  trim_scope = 'inner', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
   mode = 'topline',  -- Line used to calculate context. Choices: 'cursor', 'topline'
 }
 
